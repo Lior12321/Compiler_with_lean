dd19e486278a3123d06d1382f7eafa55b3ad4f3b
Lior12321 <lior.m.masas@gmail.com>
2025-03-25 16:39:45 +0200

    check

--- Main.lean
+++ Main.lean
@@ -2,5 +2,10 @@ import Compiler.Compiler
 
 def main : IO Unit :=
   do
+    let x: Int:= 1
+    let y: Int:= 2
+    let z: Int:= x + y
+    let message: String:= "testing"
     IO.println "Running program..."
+    IO.println s!"Message: {message} 1+2={z}"
     IO.println "Hello, World!"
