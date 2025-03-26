3fdb9d60b70e053b4b8cec12d79c8a1b525813b0
Lior12321 <lior.m.masas@gmail.com>
2025-03-26 15:43:14 +0200

    basic functions

--- Main.lean
+++ Main.lean
@@ -3,4 +3,11 @@ import Compiler.Compiler
 def main : IO Unit :=
   do
     IO.println "Running program..."
-    IO.println "Hello, World!"
+
+    let x : Int := 1
+    let y : Int := 2
+    let z : Int := x + y
+    IO.println s! "{x} + {y} = {z}"
+
+    let message : String := "Hello, World!"
+    IO.println s!"Message: {message}"
